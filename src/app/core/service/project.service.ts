3c5b3a68a90377e645a046967ee07af61e6c68be
arturocas98 <arturocastro98@hotmail.com>
2026-04-01 10:46:51 -0500

    Update project.service.ts

--- src/app/core/service/project.service.ts
+++ src/app/core/service/project.service.ts
@@ -1,28 +1,33 @@
-import { Injectable } from "@angular/core";
-import {Project} from "src/app/shared/models/project";
-import {map, Observable, BehaviorSubject, tap} from "rxjs";
-import {ApiService} from "./api.service";
-import {ProjectSummaryData} from "../../shared/models/summary-response";
+import { Injectable } from '@angular/core';
+import { Project } from 'src/app/shared/models/project';
+import { map, Observable, BehaviorSubject, tap } from 'rxjs';
+import { ApiService } from './api.service';
+import { ProjectSummaryData } from '../../shared/models/summary-response';
 import {
-  ProjectMemberRequest, ProjectMemberUpdateRequest,
+  ProjectMemberRequest,
+  ProjectMemberUpdateRequest,
   ProjectRequest,
-  UpdateProjectRequest
-} from "../../shared/models/projects-models/project-request";
-import {ProjectCreationData} from "../../shared/models/projects-models/project-create-response";
-import {ProjectResponse} from "../../shared/models/projects-models/ProjectResponse";
+  UpdateProjectRequest,
+} from '../../shared/models/projects-models/project-request';
+import { ProjectCreationData } from '../../shared/models/projects-models/project-create-response';
+import { ProjectResponse } from '../../shared/models/projects-models/ProjectResponse';
 import {
   IncidenceDetail,
   IncidenceModel,
   TaskCreateModelRequest,
-  TaskUpdateModelRequest
-} from "../../shared/models/task-models/task-create-model";
-import {unassignedUsersData} from "../../shared/models/auth";
-import {CommentResponse} from "../../shared/models/task-models/CommentResponse";
-import {Notification} from "../../shared/models/notification-models/Notification-model";
-import {MessageResource, MessageRequest, MessageUpdateRequest} from "../../shared/models/projects-models/message-models";
+  TaskUpdateModelRequest,
+} from '../../shared/models/task-models/task-create-model';
+import { unassignedUsersData } from '../../shared/models/auth';
+import { CommentResponse } from '../../shared/models/task-models/CommentResponse';
+import { Notification } from '../../shared/models/notification-models/Notification-model';
+import {
+  MessageResource,
+  MessageRequest,
+  MessageUpdateRequest,
+} from '../../shared/models/projects-models/message-models';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class ProjectService {
   private projectsSubject = new BehaviorSubject<Project[]>([]);
@@ -34,9 +39,7 @@ export class ProjectService {
   constructor(private apiService: ApiService) {}
 
   getAll(): Observable<Project[]> {
-
     if (this.projectsSubject.value.length === 0) {
-
       this.apiService
         .get<any[]>('projects')
         .pipe(
@@ -44,9 +47,7 @@ export class ProjectService {
           tap(projects => this.projectsSubject.next(projects))
         )
         .subscribe();
-
     } else {
-
       // refresco en segundo plano
       this.apiService
         .get<any[]>('projects')
@@ -55,73 +56,54 @@ export class ProjectService {
           tap(projects => this.projectsSubject.next(projects))
         )
         .subscribe();
-
     }
 
     return this.projects$;
   }
 
   getAllNotification(): Observable<Notification[]> {
-
     if (this.notificationsSubject.value.length === 0) {
-
       // primera carga
       this.apiService
         .get<Notification[]>(`notification`)
-        .pipe(
-          tap(notifications => this.notificationsSubject.next(notifications))
-        )
+        .pipe(tap(notifications => this.notificationsSubject.next(notifications)))
         .subscribe();
-
     } else {
-
       // refresco en segundo plano
       this.apiService
         .get<Notification[]>(`notification`)
-        .pipe(
-          tap(notifications => this.notificationsSubject.next(notifications))
-        )
+        .pipe(tap(notifications => this.notificationsSubject.next(notifications)))
         .subscribe();
-
     }
 
     return this.notifications$;
   }
 
-  getNotification(idNotification:number): Observable<Notification> {
+  getNotification(idNotification: number): Observable<Notification> {
     return this.apiService.get<Notification>(`notification/${idNotification}`);
   }
 
-  UpdateNotification(idNotification:number): Observable<Notification> {
+  UpdateNotification(idNotification: number): Observable<Notification> {
     return this.apiService.pat<Notification>(`notification/${idNotification}/read`);
   }
 
   getProjectSummary(projectId: number): Observable<ProjectSummaryData | null> {
-
     if (!this.projectSummaryCache.has(projectId)) {
-
       const subject = new BehaviorSubject<ProjectSummaryData | null>(null);
       this.projectSummaryCache.set(projectId, subject);
 
       this.apiService
         .get<ProjectSummaryData>(`projects/${projectId}/summary`)
-        .pipe(
-          tap(summary => subject.next(summary))
-        )
+        .pipe(tap(summary => subject.next(summary)))
         .subscribe();
-
     } else {
-
       const subject = this.projectSummaryCache.get(projectId)!;
 
       // refresco en segundo plano
       this.apiService
         .get<ProjectSummaryData>(`projects/${projectId}/summary`)
-        .pipe(
-          tap(summary => subject.next(summary))
-        )
+        .pipe(tap(summary => subject.next(summary)))
         .subscribe();
-
     }
 
     return this.projectSummaryCache.get(projectId)!.asObservable();
@@ -135,10 +117,9 @@ export class ProjectService {
     return this.apiService.get<{ role_type: string }>(`projects/${projectId}/my-role`);
   }
 
-  getOneTask(projectId: number, taskId:number): Observable<IncidenceDetail> {
+  getOneTask(projectId: number, taskId: number): Observable<IncidenceDetail> {
     return this.apiService.get<IncidenceDetail>(`projects/${projectId}/incidences/${taskId}`);
   }
-
 
   createTask(projectData: TaskCreateModelRequest, projectId: number): Observable<IncidenceModel> {
     return this.apiService.post<IncidenceModel>(`projects/${projectId}/incidences`, projectData);
@@ -156,25 +137,28 @@ export class ProjectService {
     return this.apiService.delete<any>(`projects/${projectId}`);
   }
 
-  addMember(projectId: number,projectMemberData: ProjectMemberRequest): Observable<any> {
+  addMember(projectId: number, projectMemberData: ProjectMemberRequest): Observable<any> {
     return this.apiService.post<any>(`projects/${projectId}/members`, projectMemberData);
   }
 
-  removeMember(projectId: number,userId: number): Observable<any> {
+  removeMember(projectId: number, userId: number): Observable<any> {
     return this.apiService.delete<any>(`projects/${projectId}/members/${userId}`);
   }
 
-  removeIncidence(projectId: number,userId: number): Observable<any> {
+  removeIncidence(projectId: number, userId: number): Observable<any> {
     return this.apiService.delete<any>(`projects/${projectId}/incidences/${userId}`);
   }
 
-  updateMember(projectId: number, memberId:number, projectMemberUpdateData: ProjectMemberUpdateRequest): Observable<any> {
+  updateMember(
+    projectId: number,
+    memberId: number,
+    projectMemberUpdateData: ProjectMemberUpdateRequest
+  ): Observable<any> {
     return this.apiService.patch<any>(`projects/${projectId}/members/${memberId}/role`, projectMemberUpdateData);
   }
 
-
-  getUnassignedUsers(projectId:number): Observable<unassignedUsersData[]> {
-    return this.apiService.get<unassignedUsersData[]>(`projects/${projectId}/unassigned-users`);
+  getUnassignedUsers(projectId: number): Observable<unassignedUsersData[]> {
+    return this.apiService.get<unassignedUsersData[]>(`projects/${projectId}/unassigned-users?page=1&per_page=1000`);
   }
 
   createProject(projectData: ProjectRequest): Observable<ProjectCreationData> {
@@ -182,22 +166,24 @@ export class ProjectService {
   }
 
   /*comentarios*/
-  getComments(projectId: number, taskId:number): Observable<CommentResponse[]> {
+  getComments(projectId: number, taskId: number): Observable<CommentResponse[]> {
     // GET /projects/{project}/incidences/{incidence}/comments
     return this.apiService
-      .get<{ data: CommentResponse, meta?: any }[]>(`projects/${projectId}/incidences/${taskId}/comments`)
+      .get<{ data: CommentResponse; meta?: any }[]>(`projects/${projectId}/incidences/${taskId}/comments`)
       .pipe(
-        map(responseArray => responseArray.map(item => {
+        map(responseArray =>
+          responseArray.map(item => {
             const comment = item.data;
             if (item.meta) {
-                comment.meta = item.meta as any;
+              comment.meta = item.meta as any;
             }
             return comment;
-        }))
+          })
+        )
       );
   }
 
-// Crear un nuevo comentario
+  // Crear un nuevo comentario
   createComment(projectId: number, taskId: number, description: string, file?: File): Observable<CommentResponse> {
     const formData = new FormData();
     formData.append('description', description);
@@ -207,45 +193,51 @@ export class ProjectService {
 
     // POST /projects/{project}/incidences/{incidence}/comments
     return this.apiService
-      .postFull<CommentResponse>(`projects/${projectId}/incidences/${taskId}/comments`, formData, { omitContentType: true })
+      .postFull<CommentResponse>(`projects/${projectId}/incidences/${taskId}/comments`, formData, {
+        omitContentType: true,
+      })
       .pipe(
         map(res => {
-            const comment = res.data;
-            if (res.meta) {
-                comment.meta = res.meta as any;
-            }
-            return comment;
+          const comment = res.data;
+          if (res.meta) {
+            comment.meta = res.meta as any;
+          }
+          return comment;
         })
       );
   }
 
-// Actualizar un comentario existente
-  updateComment(projectId: number, taskId: number, commentId: number, description: string): Observable<CommentResponse> {
+  // Actualizar un comentario existente
+  updateComment(
+    projectId: number,
+    taskId: number,
+    commentId: number,
+    description: string
+  ): Observable<CommentResponse> {
     // PUT /projects/{project}/incidences/{incidence}/comments/{commentId}
     return this.apiService
-      .put<{ data: CommentResponse }>(`projects/${projectId}/incidences/${taskId}/comments/${commentId}`, { description })
+      .put<{
+        data: CommentResponse;
+      }>(`projects/${projectId}/incidences/${taskId}/comments/${commentId}`, { description })
       .pipe(
         map(res => res.data) // extraemos solo data
       );
   }
 
-// Eliminar un comentario
+  // Eliminar un comentario
   deleteComment(projectId: number, taskId: number, commentId: number): Observable<void> {
     // DELETE /projects/{project}/incidences/{incidence}/comments/{commentId}
-    return this.apiService
-      .delete<void>(`projects/${projectId}/incidences/${taskId}/comments/${commentId}`);
+    return this.apiService.delete<void>(`projects/${projectId}/incidences/${taskId}/comments/${commentId}`);
   }
 
   // --- PROJECT MESSAGES (CHAT) ---
 
   getProjectMessages(projectId: number): Observable<MessageResource[]> {
-    return this.apiService.get<any>(`projects/${projectId}/messages`)
-      .pipe(map(res => res.data || res));
+    return this.apiService.get<any>(`projects/${projectId}/messages`).pipe(map(res => res.data || res));
   }
 
   getProjectFiles(projectId: number): Observable<any[]> {
-    return this.apiService.get<any>(`projects/${projectId}/files`)
-      .pipe(map(res => res.data || res));
+    return this.apiService.get<any>(`projects/${projectId}/files`).pipe(map(res => res.data || res));
   }
 
   createProjectMessage(projectId: number, payload: MessageRequest, file?: File): Observable<MessageResource> {
@@ -255,22 +247,26 @@ export class ProjectService {
       if (payload.text) formData.append('text', payload.text);
       if (payload.message_id) formData.append('message_id', payload.message_id.toString());
       formData.append('file', file);
-      
-      return this.apiService.postFull<any>(`projects/${projectId}/messages`, formData, { omitContentType: true })
+
+      return this.apiService
+        .postFull<any>(`projects/${projectId}/messages`, formData, { omitContentType: true })
         .pipe(map(res => res.data || res));
     }
 
-    return this.apiService.post<any>(`projects/${projectId}/messages`, payload)
-      .pipe(map(res => res.data || res));
+    return this.apiService.post<any>(`projects/${projectId}/messages`, payload).pipe(map(res => res.data || res));
   }
 
-  updateProjectMessage(projectId: number, messageId: number, payload: MessageUpdateRequest): Observable<MessageResource> {
-    return this.apiService.put<any>(`projects/${projectId}/messages/${messageId}`, payload)
+  updateProjectMessage(
+    projectId: number,
+    messageId: number,
+    payload: MessageUpdateRequest
+  ): Observable<MessageResource> {
+    return this.apiService
+      .put<any>(`projects/${projectId}/messages/${messageId}`, payload)
       .pipe(map(res => res.data || res));
   }
 
   deleteProjectMessage(projectId: number, messageId: number): Observable<void> {
     return this.apiService.delete<void>(`projects/${projectId}/messages/${messageId}`);
   }
-
 }
